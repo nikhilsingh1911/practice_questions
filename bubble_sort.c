3720e5500312101c3867e504e06131c7b9dbc120
nikhilsingh1911 <115785542+nikhilsingh1911@users.noreply.github.com>
2023-12-12 20:46:27 +0530

    a small change

--- bubble_sort.c
+++ bubble_sort.c
@@ -16,6 +16,9 @@ int main()
     printf("Sorting will be done, Now:");
     for (i = 0; i < n - 1; i++)
     {
+        // to check that if there is any swap done or not.. if no swap is done then
+        //  thats means array is sorted but code will be excuted furhter so to check that we will add this new instructiosn
+        int flag=0;
         for (j = 0; j < n - i - 1; j++)
         {
             if (a[j] > a[j + 1])
@@ -23,7 +26,11 @@ int main()
                 temp = a[j];
                 a[j] = a[j + 1];
                 a[j + 1] = temp;
+                flag = 1;
             }
+        }
+        if (flag==0){
+            break;
         }
     }
     printf("Sorted array is: ");
