e099c16e31c0774147549289f6bc9041895578c4
nikhilsingh1911 <115785542+nikhilsingh1911@users.noreply.github.com>
2023-12-14 23:22:07 +0530

    .

--- merge_sort.c
+++ merge_sort.c
@@ -143,13 +143,13 @@ int main()
     int arr[] = {12, 11, 13, 5, 6, 7};
     int arr_size = sizeof(arr) / sizeof(arr[0]);
     printf("Given array is \n");
+    int i;
     for (i = 0; i < arr_size; i++)
     {
         printf(" %d ", arr[i]);
     }
     mergeSort(arr, 0, arr_size - 1);
     printf("\nSorted array is \n");
-    int i;
     // printArray(arr, arr_size);
     for (i = 0; i < arr_size; i++)
     {
