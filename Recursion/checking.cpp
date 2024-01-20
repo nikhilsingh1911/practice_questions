e70d9a10f0c3fcaef695df424e3885dfb018d353
nikhilsingh1911 <115785542+nikhilsingh1911@users.noreply.github.com>
2024-01-20 14:01:31 +0530

    .

--- Recursion/checking.cpp
+++ Recursion/checking.cpp
@@ -11,9 +11,18 @@ void print(int n){
 }
 
 int main(){
-    int n;
-    cout<<"enter:";
-    cin>>n;
-
-    print(n);
+    int numbers[5];
+    int *p;
+    p= numbers;
+    *p=10;
+    p++;
+    *p=20;
+    p= &numbers[2];
+     *p=30;
+    p= numbers+3;
+    *p=40;
+    p= numbers;
+    *(p+4)=50;
+    for(int i=0;i<5;i++)
+        cout<<numbers[i]<<",";
 }
