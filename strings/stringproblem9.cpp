51e49195fd742a6682ab75a07fec000952ee7881
nikhilsingh1911 <115785542+nikhilsingh1911@users.noreply.github.com>
2024-03-17 15:26:42 +0530

    ll

---
+++ strings/stringproblem9.cpp
@@ -0,0 +1,19 @@
+// remove all the occurence of the substring from a string..
+
+#include <iostream>
+using namespace std;
+
+void deleteSubstring(string s, string sub){
+    
+}
+
+int main()
+{
+    string s;
+    cout << "Enter the main string: " << endl;
+    cin >> s;
+    string substr;
+    cout << "Enter the sub string: " << endl;
+    cin >> substr;
+    deleteSubstring(s, substr);
+}
