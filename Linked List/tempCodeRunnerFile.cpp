e0036e26410ba70972b27dc143eff992ea19c267
nikhilsingh1911 <115785542+nikhilsingh1911@users.noreply.github.com>
2024-04-25 22:46:38 +0530

    /

--- Linked List/tempCodeRunnerFile.cpp
+++
@@ -1,4 +0,0 @@
-;
-    // insertAtHead(head, 5);
-    // insertAtHead(head, 4);
-    // insertAtHead(head, 3);
---
+++ Linked Lists/linklist1.cpp
@@ -0,0 +1,4 @@
+#include<iostream>
+using namespace std;
+
+//Linked List :- It is a type of linear data structure and collections of nodes.
